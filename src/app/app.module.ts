1ce1d2f50b65d9729738a085438bb96bf4ba3cc9
Kabir Sondhi <ksondhi78@gmail.com>
2017-08-04 22:53:41 -0400

    Hooked up the angular router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SpreadsheetComponent } from './spreadsheet/spreadsheet.component';
+import { HeaderComponent } from './header/header.component';
+
+export const ROUTES: Routes = [
+  {path: 'spreadsheet', component: SpreadsheetComponent},
+  {path: '', redirectTo: '/spreadsheet', pathMatch: 'full'},
+  {path: '**', redirectTo: '/spreadsheet', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpreadsheetComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
